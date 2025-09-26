6f8c7de0a3c4d90b62a150cf2706793655e65801
pranberry <180306401+pranberry@users.noreply.github.com>
2025-09-25 20:03:55 -0400

    improved logging and returned response

--- Notes.md
+++ Notes.md
@@ -1,18 +1,31 @@
-# Solution Notes (during and post):
+# Solution Notes (during and post)
+
+## Challenges
 
 - determining the right level of abstraction is very important
   - this will determine the shape of validation logic
-    - every request requires a file be opened, matrix loaded, and matrix validate
-    - if any of these operations fail, bail on the request
-- I could easily operate on `[][]string`, converting values to int for the arithematic funcs(), but...
+- every request requires a file be opened, matrix loaded, and matrix validate
+  - if any of these operations fail, bail on the request
+
+- I could easily operate on `[][]string`, converting values to int for the arithmetic funcs(), but...
   - creating a `type Matrix` would enable more complex behaviour
   - provide type safety
   - better validation of a given matrix
   - better testing, all on int type...
   - HOWEVER, will not be able to use strings.Join(row, ",") for doing the printing....big hit imo
+
 - watchout for scope creep:
   - the requirement is pretty straight-forward
-- no concurrency here
-- not enforcing HTTP Request types
-- returning all requests as string not JSON
-- tests added
+
+## What missing 
+
+- any need for concurrency features
+- not enforcing access by HTTP Request types. GET, POST, etc. all have the same behaviour
+- JSON response. Was not specified, returning all requests as string
+- logging middleware.
+  - would be nice to just define and wrap
+  - channel logging
+
+## Assumptions
+
+- treating empty values (eg: `1,,2`) as legitimate.
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -9,76 +9,130 @@ import (
 
 // Print back the matrix
 func Echo(w http.ResponseWriter, r *http.Request) {
-	// expect a file with the key "file" in form data
+
+	// before and after logging
+	log.Printf("REQUEST: method=%v on url=%v from remote=%v", r.Method, r.URL.Path, r.RemoteAddr)
+	reqResp := http.StatusBadRequest
+	defer func(){
+		log.Printf("RESPONSE: status=%d on url=%v on %v from %v", reqResp, r.Method, r.URL.Path, r.RemoteAddr)
+	}()
+	
+	// get matrix
 	matrix, err := matrix.NewMatrix(r)
 	if err != nil{
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, err.Error(), reqResp)
 		return
 	}
 
+	// write and return response
+	w.Header().Set("Content-Type", "text/csv")
+	reqResp = http.StatusOK	
+	w.WriteHeader(reqResp)
 	fmt.Fprint(w, matrix.Echo())
 }
 
 // Invert/Transpose a NxN matrix...rows become columns, columns become rows
 func Transpose(w http.ResponseWriter, r *http.Request){
 
-	log.Println("Request: /invert from", r.RemoteAddr)
+	log.Printf("REQUEST: method=%v on url=%v from remote=%v", r.Method, r.URL.Path, r.RemoteAddr)
+	reqResp := http.StatusBadRequest
+	defer func(){
+		log.Printf("RESPONSE: status=%d on url=%v on %v from %v", reqResp, r.Method, r.URL.Path, r.RemoteAddr)
+	}()
 
 	matrix, err := matrix.NewMatrix(r)
 	if err != nil{
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, err.Error(), reqResp)
 		return
 	}
 
+	// call to actually mutate (transpose) the matrix
 	matrix.Transpose()
-	
+
+	// write and return response
+	w.Header().Set("Content-Type", "text/csv")
+	reqResp = http.StatusOK	
+	w.WriteHeader(reqResp)
 	fmt.Fprint(w, matrix.Echo())
 }
 
 
 // Returns the flattened representation of the matrix
 func Flatten(w http.ResponseWriter, r *http.Request){
-	log.Println("Request: /flatten from", r.RemoteAddr)
+	
+	log.Printf("REQUEST: method=%v on url=%v from remote=%v", r.Method, r.URL.Path, r.RemoteAddr)
+	reqResp := http.StatusBadRequest
+	defer func(){
+		log.Printf("RESPONSE: status=%d on url=%v on %v from %v", reqResp, r.Method, r.URL.Path, r.RemoteAddr)
+	}()
 
 	matrix, err := matrix.NewMatrix(r)
 	if err != nil{
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, err.Error(), reqResp)
 		return
 	}
 	
+	reqResp = http.StatusOK
+	w.Header().Set("Content-Type", "text/csv")
+	w.WriteHeader(reqResp)
+	// flattened response returned here
 	fmt.Fprint(w, matrix.Flatten())
 }
 
 
 // Returns the sum of all values in a matrix
 func Addition(w http.ResponseWriter, r *http.Request){
-	log.Println("Request: /add from", r.RemoteAddr)
+	
+	log.Printf("REQUEST: method=%v on url=%v from remote=%v", r.Method, r.URL.Path, r.RemoteAddr)
+	reqResp := http.StatusBadRequest
+	defer func(){
+		log.Printf("RESPONSE: status=%d on url=%v on %v from %v", reqResp, r.Method, r.URL.Path, r.RemoteAddr)
+	}()
+
 	matrix, err := matrix.NewMatrix(r)
 	if err != nil{
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, err.Error(), reqResp)
 		return
 	}
+
+	// calculate sum
 	sum, err := matrix.Add()
 	if err != nil{
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, err.Error(), reqResp)
 		return
 	}
+	
+	reqResp = http.StatusOK
+	w.Header().Set("Content-Type", "text/csv")
+	w.WriteHeader(reqResp)
 	fmt.Fprint(w, sum)
 }
 
 // Returns the product of all values in a matrix
 func Multiply(w http.ResponseWriter, r *http.Request){
-	log.Println("Request: /mul from", r.RemoteAddr)
+	
+	log.Printf("REQUEST: method=%v on url=%v from remote=%v", r.Method, r.URL.Path, r.RemoteAddr)
+	reqResp := http.StatusBadRequest
+	defer func(){
+		log.Printf("RESPONSE: status=%d on url=%v on %v from %v", reqResp, r.Method, r.URL.Path, r.RemoteAddr)
+	}()
+
 	matrix, err := matrix.NewMatrix(r)
 	if err != nil{
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, err.Error(), reqResp)
 		return
 	}
+	
+	// calculate product
 	prod, err := matrix.Multiply()
 	if err != nil{
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, err.Error(), reqResp)
 		return
 	}
+	
+	reqResp = http.StatusOK
+	w.Header().Set("Content-Type", "text/csv")
+	w.WriteHeader(reqResp)
 	fmt.Fprint(w, prod)
 }
 
